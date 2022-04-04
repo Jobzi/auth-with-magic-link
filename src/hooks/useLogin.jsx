91f86cbbd471dd3b6ff74e1a16bb496928072b5a
Jipson Murillo <38593785+Jobzi@users.noreply.github.com>
2022-04-04 19:01:40 +0000

    login with magic link

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,23 +1,46 @@
 import { Magic } from 'magic-sdk';
 import { OAuthExtension } from '@magic-ext/oauth';
 
-const magic = new Magic('pk_live_5BE2BF80F5020B3D', {
+const magic = new Magic('pk_live_0D019DEC03807D93', {
   extensions: [new OAuthExtension()],
 });
 
 function useLogin() {
   const loginWithGoogle = async () => {
-    const result = await magic.oauth.loginWithRedirect({
+    await magic.oauth.loginWithRedirect({
       provider: 'google' /* 'google', 'facebook', 'apple', or 'github' */,
       redirectURI:
-        'https://auth.magic.link/v1/oauth2/w2xdYaX3MIl52e5uI0M_NDkQCUtGUGYNzw_bZzmobQE=/callback',
-      scope: ['user:email'] /* optional */,
+        'https://jobzi-auth-with-magic-link-p6xxg66537555-3000.githubpreview.dev/',
+    }).then((res)=>{
+      console.log(res);
     });
-    console.log(result);
   };
+
+  const information = async () =>{
+    console.log("here")
+    const resul1 = await magic.user.isLoggedIn()
+    console.log(resul1)
+    //  const resul2 = await magic.oauth.getRedirectResult();
+    //  console.log(resul2)
+    //  const profile = JSON.stringify(resul2.oauth.userInfo, undefined, 2);
+    //  alert(profile);
+    const resul3 = await magic.user.getMetadata();
+    console.log(resul3)
+     console.log(await magic.user)
+    const resul4 = await magic.user.getIdToken()
+    console.log(resul4)
+    console.log("fin")
+  }
+  
+  const logOut = async () => {
+    await magic.user.logout();
+    alert("LogOut")
+  }
 
   return {
     loginWithGoogle,
+    information,
+    logOut,
   };
 }
 
--- src/view/login.jsx
+++ src/view/login.jsx
@@ -1,17 +1,24 @@
 import useLogin from '../hooks/useLogin';
 
 function Login() {
-  const { loginWithGoogle } = useLogin();
+  const { loginWithGoogle, information,logOut } = useLogin();
 
   const handleClink = (e) => {
     e.preventDefault();
     console.log('HERE');
     loginWithGoogle();
   };
+  const handleClinkInformation = (e) => {
+    e.preventDefault();
+    console.log('HERE');
+    information();
+  };
   return (
     <>
       <h1>Auth with Google</h1>
       <button onClick={handleClink}>Google</button>
+      <button onClick={handleClinkInformation}>handleClinkInformation</button>
+      <button onClick={logOut}>log</button>
     </>
   );
 }
